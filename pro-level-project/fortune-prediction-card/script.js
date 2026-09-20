138a863999cfc084c8cb4192fcaa33ede12e3fb6
Khushi Singh <khu4656@gmail.com>
2026-09-20 23:45:20 +0530

    Refactor meter

--- pro-level-project/fortune-prediction-card/script.js
+++ pro-level-project/fortune-prediction-card/script.js
@@ -17,12 +17,14 @@ const fortune = [
     "Are tum to bilkul lallulaal😂😂😂 ho thoda personality ko change karu achhi chhez hi positive engery deti hai thoda shudaro khud ko thik.",
     "Jaise kutte ki dul kabhi shidi nahi ho skti hai vaise tum kabhi sudar nahi sakte ho na  😡😡😡 roj kyu glti karte ho aaj tumhne daant padegi kisi se bhi jarur kah lena thik galti tumhari hi hai.",
     "Hiii chandulaal inta hosiyaar kyu bante ho 🤭🤭 . Lekin ek baat or tum bhut cute ho ",
-    "Apki muskaan bhut achhi hai. 😘😘"
+    "Apki muskaan bhut achhi hai. 😘😘",
+    "Mujhe dikh raha hai ki jald hi aapko kuch bohot tasty khane ko milne wala hai!"
 ]
 
 
 function fortuneTeller(){
-
+   // yaha per mujhew jkaam karn hai
+    document.getElementById("fortune-result").classList.remove("hidden");
     const userName = document.getElementById("username").value;
     const randomIndex = Math.floor(Math.random()*fortune.length);
     const userFortune = fortune[randomIndex];
@@ -30,9 +32,60 @@ function fortuneTeller(){
 
     let fortuneDetail = (userName === "") 
         ? `Please Enter your name first` 
-        : `Hii ${userName} \n\nUniverse ne apke sitare padhe or pata hai kya batya hai apke bagye ne khud padh lo \n\n"${userFortune}"`;
+        : `Hii ${userName} \n\nUniverse ne apke sitare padhe or pata hai kya batya hai apke bhagye ne khud padh lo \n\n"${userFortune}"`;
     
     fortuneMessage.innerText = fortuneDetail 
 }
 
+function fortuneMeter(){
+    let name = document.getElementById("name").value.trim();
+    document.getElementById("meter-result").classList.remove("hidden");
+    document.getElementById("container-box").classList.add("hidden")
+    
+    let nameScore = 0;
+    for(let i=0; i<name.length; i++){
+        nameScore += name.charCodeAt(i);
+    }
+    let success = (nameScore % 56) + 40;          
+    let happy = ((nameScore * 2) % 50) + 50;     
+    let mind = ((nameScore * 3) % 39) + 60;       
+    let love = ((nameScore * 7) % 30) + 50;       
+    let money = ((nameScore * 5) % 50) + 30;      
+    let sadness = ((nameScore + 7) % 41) + 5;     
+    let shockingNews = ((nameScore * 4) % 71) + 10; 
+    document.getElementById("greet").innerText = `🌟 Welcome ${name} ! \n Here is your personalized Destiny Card:`;
+
+
+    // value increase
+
+    document.getElementById("mind-value").innerText = mind + "%";
+    document.getElementById("money-value").innerText = money + "%";
+    document.getElementById("love-value").innerText = love + "%";
+    document.getElementById("sadness-value").innerText = sadness + "%";
+    document.getElementById("happiness-value").innerText = happy + "%";
+    document.getElementById("shocking-news-value").innerText = shockingNews + "%";
+    document.getElementById("success-value").innerText = success + "%";
+
+
+    // bar increase decrease
+
+    setTimeout(() => {
+    document.getElementById("mind-bar").style.width = mind + "%";
+    document.getElementById("money-bar").style.width = money + "%";
+    document.getElementById("love-bar").style.width = love + "%";
+    document.getElementById("sadness-bar").style.width = sadness + "%";
+    document.getElementById("happiness-bar").style.width = happy + "%";
+    document.getElementById("success-bar").style.width = success + "%";
+    document.getElementById("shocking-bar").style.width = shockingNews + "%";
+
+    }, 100)
+
+}
+
+// go back button
+
+function goBack(){
+    document.getElementById("meter-result").classList.add("hidden");
+    document.getElementById("container-box").classList.remove("hidden");
+}
 
